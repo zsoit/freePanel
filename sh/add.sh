af512d9b3b2cdacaf27ea95eb9b6a50e7023c269
jakun <wsparcie.exe@gmail.com>
2023-04-17 10:04:37 +0200

    freePanel - sh

--- sh/add.sh
+++ sh/add.sh
@@ -11,31 +11,32 @@ domain=$3
 
 if [ -z "$name"  ] || [ -z "$password"  ] || [ -z "$domain"  ]
 then
-    echo "Variables error !"
+    echo "ERROR: Variables error !"
     exit 1
 fi
-
 
 echo $name $password $domain
 
-# USUWA JESLI JEST
-userdel -f ${name}
-rm -rf /www/${name}/
-rm -rf /etc/apache2/sites-available/${domain}.conf
-
-#UZYTKOWNIK
-groupadd -f ${name}
-useradd -g ${name} ${name}``
-usermod -g sftp_users -d /www/${name} -s /dev/null ${name}
-usermod -g www-data $name
-echo $name:$password | chpasswd
-
-#KATALOG
-if [ -d "/www/${name}/" ]
-then
-    echo "Katalog istnieje!"
+if id "$name" >/dev/null 2>&1; then
+    echo "ERROR: THE USER $name EXISTS"
     exit 1
 fi
+
+if [ -d "/www/${name}/" ]
+then
+    echo "ERROR: THE CATALOG EXISTS!"
+    exit 1
+fi
+
+# USERS
+groupadd -f ${name}
+useradd -g ${name} ${name}
+usermod -g sftp_users -d /www/${name} -s /dev/null ${name}
+usermod -a -G www-data $name
+echo -e "$password\n$password" | passwd $name
+# echo "${name}:${password}" | chpasswd
+
+
 sudo mkdir /www/${name}/
 sudo mkdir /www/${name}/public_html
 sudo mkdir /www/${name}/private
@@ -53,4 +54,7 @@ echo "
 </VirtualHost>
 " > /etc/apache2/sites-available/${domain}.conf
 cd /etc/apache2/sites-available/; a2ensite ${domain}.conf
+
+echo "User $name has been added!"
+
 sleep 1s && systemctl reload apache2
